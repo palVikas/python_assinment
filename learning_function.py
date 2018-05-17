a1a16f0551325dd63226e537a6474542590adc30
vikas <vikaspal807657@gmail.com>
2018-05-16 21:36:01 -0700

    modifed in learning_function.py

--- learning_function.py
+++ learning_function.py
@@ -1,5 +1,5 @@
 #write a programme using function to sum of given number using user
-"""first_number = int(input("enter the first number: "))
+first_number = int(input("enter the first number: "))
 second_number = int(input("enter the second number: "))
 
 def add_number(a,b):
@@ -7,25 +7,23 @@ def add_number(a,b):
 
 
 sum = add_number(first_number,second_number);
-print (sum)"""
+print (sum)
 
 #write a programe to prite a series a given number
-
-"""number = int(input("enter a number: "))
+number = int(input("enter a number: "))
 for num in range(1,number+1):
     print(num)
-"""
+
 
 #factorial of a given number
 
-"""number = int(input("enter a number you want to factorial: "))
+number = int(input("enter a number you want to factorial: "))
 
 def factorial(num):
     if (num==1 or num ==0):
         return 1
     return num*factorial(num-1)
 
-print(factorial(number))"""
 
 #write a programe to print ap series of given number
 
