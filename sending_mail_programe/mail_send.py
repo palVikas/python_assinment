280068add0adbaba3e32dcff4f0981ac2b517b42
vikas <vikaspal807657@gmail.com>
2018-05-17 03:31:45 -0700

    get_email_id function built in sending mail programe

--- sending_mail_programe/mail_send.py
+++ sending_mail_programe/mail_send.py
@@ -1,6 +1,9 @@
 # using smtplib
 import smtplib
 
+
+
+"""
 connection = smtplib.SMTP('smtp.gmail.com', 587)
 connection.ehlo()
 connection.starttls()
@@ -9,3 +12,41 @@ connection.sendmail('vikas807657@gmail.com','vikaspal807657@gmail.com','hello ho
 connection.quit()
 
 print ("connect successfully msg send")
+
+"""
+
+
+#input admin email_id_
+
+email_id = input("enter your email id : ")
+print (email_id)
+password =  input("enter your password: ")
+print (password)
+
+
+def is_valid_email_id(email_id):
+    domain_name = ["gmail",'yahoo','outlook','hotmail']
+    if '@' in email_id and '.' in email_id :
+        symbol_pos = email_id.find('@')
+        dotcom_pos = email_id.find(".com")
+        domain = email_id[symbol_pos+1 : dotcom_pos]
+        print(domain)
+        if domain in domain_name:
+            return  email_id , domain
+        else:
+            print('we dont provide service '+ domain)
+            print ('we provide only services : gmail/yahoo/outlook/hotmail')
+    
+
+
+
+
+    else:
+         print("invalid email id")
+
+
+email , domain = is_valid_email_id(email_id)
+print(email)
+print(domain)
+print(domain)
+
